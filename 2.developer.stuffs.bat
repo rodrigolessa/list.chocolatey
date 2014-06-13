c653c9047ace7e30654837c0a528adfd47cd3b0d
rodrigolsr <rodrigolsr@gmail.com>
2014-06-13 20:07:39 -0300

    Add arquivos

---
+++ 2.developer.stuffs.bat
@@ -0,0 +1,39 @@
+cinst PowerShell
+cinst git.install
+cinst SublimeText3
+cinst nodejs.install
+cinst putty
+cinst filezilla
+cinst fiddler
+cinst paint.net
+cinst NuGet.CommandLine
+cinst Compass
+cinst winmerge
+---------------------------------------------------
+
+cinst 7zip.install
+cinst javaruntime
+cinst GoogleChrome
+cinst Firefox
+cinst flashplayeractivex
+cinst adobereader
+cinst dropbox
+cinst skype
+cinst keepass
+cinst teamviewer
+?? ChromeWebDriver
+---------------------------------------------------
+
+cinst ruby
+cinst python
+cinst mysql.workbench
+cinst hg
+---------------------------------------------------
+
+cinst ccleaner
+cinst virtualbox
+cinst notepadplusplus.install
+cinst ConEmu
+cinst PDFCreator
+cinst calibre
+cinst libreoffice
